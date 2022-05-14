25e288f58026ac5addbc245e9428e0c9b7091228
Freeze_Dolphin <freeze-dolphin@outlook.com>
2022-05-14 03:41:53 +0000

    .

--- src/main.cr
+++ src/main.cr
@@ -3,19 +3,99 @@ require "crsfml"
 module CrystallizedEnigma
   VERSION = "0.1.0"
 
-  window = SF::RenderWindow.new(SF::VideoMode.new(800, 640), "Enigma")
+  def draw_char(font, c, x, y, size = 16, color = SF::Color::White)
+    text = SF::Text.new
+    text.font = font
+    text.string = c
+    text.character_size = size
+    text.color = color
+    text.position = {x, y}
+
+    window.draw text
+  end
+
+  def trans_scene(from, to)
+    scene[from] = false
+    scene[to] = true
+    window.clear SF::Color::Black
+  end
+
+  window = SF::RenderWindow.new(
+    SF::VideoMode.new(800, 640), "Enigma",
+    settings: SF::ContextSettings.new(depth: 24, antialiasing: 8))
 
   window.vertical_sync_enabled = true
+
+  scene = {"main" => true, "tiled" => false}
 
   while window.open?
     while event = window.poll_event
       case event
       when SF::Event::Closed
         window.close
+      when SF::Event::KeyReleased
+        if event.code == SF::Keyboard::Escape
+          window.close
+          if event.code == SF::Keyboard::Enter
+            window.clear SF::Color::Black
+            trans_scene("main", "tiled")
+          end
+        end
       end
+
+      window.clear SF::Color::Black
+
+      scene.each do |e|
+        if e[1]
+          case e[0]
+          when "main"
+            font_wqy = SF::Font.from_file "wqy-microhei.ttc"
+
+            text = SF::Text.new
+            text.font = font_wqy
+            text.string = "Hello, wqy!\n中文测试"
+            text.character_size = 24
+            text.color = SF::Color::White
+
+            window.draw text
+
+            color_vortex = SF::VertexArray.new(SF::Quads, 4)
+
+            color_vortex[0] = SF::Vertex.new(SF.vector2(50, 450), SF::Color::Red)
+            color_vortex[1] = SF::Vertex.new(SF.vector2(50, 150), SF::Color::Blue)
+            color_vortex[2] = SF::Vertex.new(SF.vector2(350, 150), SF::Color::Green)
+            color_vortex[3] = SF::Vertex.new(SF.vector2(350, 450), SF::Color::Yellow)
+
+            window.draw color_vortex
+
+            text_vortex_title = SF::Text.new
+            text_vortex_title.font = font_wqy
+            text_vortex_title.string = "[RGYB Vortex]"
+            text_vortex_title.character_size = 16
+            text_vortex_title.color = SF::Color::White
+            text_vortex_title.position = {60, 130}
+
+            window.draw text_vortex_title
+
+            box = SF::VertexArray.new(SF::LinesStrip, 5)
+            box[0] = SF::Vertex.new(SF.vector2(45, 455), SF::Color::White)
+            box[1] = SF::Vertex.new(SF.vector2(45, 145), SF::Color::White)
+            box[2] = SF::Vertex.new(SF.vector2(355, 145), SF::Color::White)
+            box[3] = SF::Vertex.new(SF.vector2(355, 455), SF::Color::White)
+            box[4] = SF::Vertex.new(SF.vector2(45, 455), SF::Color::White)
+
+            window.draw box
+
+            draw_char(font_wqy, "R", 50, 430)
+            draw_char(font_wqy, "B", 50, 150)
+            draw_char(font_wqy, "G", 340, 150)
+            draw_char(font_wqy, "Y", 340, 430, size = 16, color = SF::Color::Black)
+          when "tiled"
+          end
+        end
+      end
+
+      window.display
     end
-
-    window.clear SF::Color::Black
-
   end
 end
